fdc66f1bbf0956b0ebdc54bbbcd70d10791839f1
Kobe Lipkens <kobelipkens@gmail.com>
2017-03-21 12:42:25 +0100

    Unused imports

--- src/Routing/DisableSimpleSearchSubscriber.php
+++ src/Routing/DisableSimpleSearchSubscriber.php
@@ -3,8 +3,6 @@
 namespace Drupal\wmsearch\Routing;
 
 use Drupal\Core\Routing\RouteSubscriberBase;
-use Drupal\Core\Routing\RoutingEvents;
-use Symfony\Component\Routing\Route;
 use Symfony\Component\Routing\RouteCollection;
 
 class DisableSimpleSearchSubscriber extends RouteSubscriberBase
